e77beec9d98a3514890b222e3cc5a7fe5fb5f713
JoseHenrique1 <jh4832184@gmail.com>
2025-03-22 17:08:29 -0300

    feat(api): deleting image if product validation fails

--- api/src/modules/product/product.controller.ts
+++ api/src/modules/product/product.controller.ts
@@ -2,73 +2,87 @@ import { RequestHandler } from "express";
 import { serviceProduct } from "./product.service.ts";
 import { validatorProduct } from "./product.validators.ts";
 import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
 
 const getProducts: RequestHandler = async (req, res) => {
-  //Pegando todos os produtos que não são do usuario autenticado
-  const products = await serviceProduct.getProducts(req.user.id);
-  res.status(200).json({products});
-  return;
+	//Pegando todos os produtos que não são do usuario autenticado
+	const products = await serviceProduct.getProducts(req.user.id);
+	res.status(200).json({ products });
+	return;
 };
 
 const getProduct: RequestHandler = async (req, res) => {
-  validatorProduct.idValidator.parse(req.params);
-  const { id: productId } = req.params;
-  
-  const product = await serviceProduct.getProduct(req.user.id, productId);
-  
-  res.status(200).json({product});
-  return;
+	validatorProduct.idValidator.parse(req.params);
+	const { id: productId } = req.params;
+
+	const product = await serviceProduct.getProduct(req.user.id, productId);
+
+	res.status(200).json({ product });
+	return;
 };
 
 const postProduct: RequestHandler = async (req, res) => {
-  const isAvailable = req.body.isAvailable === "true" ? true : false;
-  const data = {
-    ...req.body,
-    isAvailable 
-  }
+	const isAvailable = req.body.isAvailable === "true" ? true : false;
+	const data = {
+		...req.body,
+		isAvailable,
+	};
 
-  validatorProduct.productPostAndPutSchema.parse(data);
+	const dataIsValid = validatorProduct.productPostAndPutSchema.safeParse(data);
+	if (!dataIsValid.success && req.file) {
+		const __filename = fileURLToPath(import.meta.url);
+		const __dirname = path.dirname(__filename);
+		const uploadDir = path.resolve(__dirname, "../../../upload");
+    
+		const oldImagePath = path.join(uploadDir, path.basename(req.file.filename));
+		if (fs.existsSync(oldImagePath)) {
+			fs.unlinkSync(oldImagePath);
+		}
+	}
 
-  if (!req.file) {
-    res.status(400).json({ msg: "Nenhum arquivo enviado." });
-    return;
-  }
+	validatorProduct.productPostAndPutSchema.parse(data);
 
-  const filePath = path.join("/upload", req.file.filename);
+	if (!req.file) {
+		res.status(400).json({ msg: "Nenhum arquivo enviado." });
+		return;
+	}
 
-  const newData = {
-    ...data,
-    image: filePath
-  };
+	const filePath = path.join("/upload", req.file.filename);
 
-  const product = await serviceProduct.postProduct(req.user.id, newData);
-  res.status(201).json({product});
-  return;
+	const newData = {
+		...data,
+		image: filePath,
+	};
+
+	const product = await serviceProduct.postProduct(req.user.id, newData);
+	res.status(201).json({ product });
+	return;
 };
 
 const putProduct: RequestHandler = async (req, res) => {
-  validatorProduct.productSchema.parse(req.body);
-  const data = req.body;
-  validatorProduct.idValidator.parse(req.params);
-  const { id: productId } = req.params;
-  const product = await serviceProduct.putProduct(req.user.id, productId, data);
-  res.status(200).json({product});
-  return;
+	validatorProduct.productSchema.parse(req.body);
+	const data = req.body;
+	validatorProduct.idValidator.parse(req.params);
+	const { id: productId } = req.params;
+	const product = await serviceProduct.putProduct(req.user.id, productId, data);
+	res.status(200).json({ product });
+	return;
 };
 
 const deleteProduct: RequestHandler = async (req, res) => {
-  validatorProduct.idValidator.parse(req.params);
-  const { id: productId } = req.params;
+	validatorProduct.idValidator.parse(req.params);
+	const { id: productId } = req.params;
 
-  const product = await serviceProduct.deleteProduct(req.user.id, productId);
-  res.status(200).json({product});
-  return;
+	const product = await serviceProduct.deleteProduct(req.user.id, productId);
+	res.status(200).json({ product });
+	return;
 };
 
 export const controllerProduct = {
-  getProducts,
-  getProduct,
-  postProduct,
-  putProduct,
-  deleteProduct,
+	getProducts,
+	getProduct,
+	postProduct,
+	putProduct,
+	deleteProduct,
 };
