1b15cc1e63b5e25497187bc6b262d1e2d48b987a
JoseHenrique1 <jh4832184@gmail.com>
2025-07-29 20:20:20 -0300

    refactor(spa): removing stack

--- spa/src/app/(home)/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-   <div className="p-8">
-    <Link href="/auth/signin">Signin</Link>
-   </div>
-  );
-}
--- spa/src/app/interest/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Interest() {
-	
-	return (
-		<div>
-			Interest
-		</div>
-	);
-}
